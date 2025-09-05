505189811d04e1daa64b70c6d19b9d62b78edd2f
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2025-09-05 22:13:58 +0300

    Add framer-motion dependency and update button icon sizes on landing page

--- app/page.tsx
+++ app/page.tsx
@@ -18,10 +18,10 @@ export default function Home() {
    <p className="text-gray-200 text-lg md:text-xl font-sans mt-8 md:max-w-2xl leading-relaxed">Welcome to Wambilianga & CO. Advocates Law Firm, commissioners for oaths and notary public. We offer a wide range of legal services that your future and business can rely on.</p>
   <div className="absolute bottom-20 left-0 right-0 px-4 flex items-center justify-between gap-4 flex-col md:flex-row w-full md:max-w-1/2">
   <Button variant="default" className="flex items-center justify-between w-full md:w-1/2 font-sans font-semibold text-lg h-12">Get Started
-   <ArrowRight size={20} />
+   <ArrowRight size={30} />
    </Button>
    <Button variant="default" className="flex items-center justify-between w-full md:w-1/2 font-sans font-semibold text-lg h-12">Free Case Evaluation
-   <BriefcaseBusiness size={20} />
+   <BriefcaseBusiness size={30} />
    </Button>
   </div>
    </div>
